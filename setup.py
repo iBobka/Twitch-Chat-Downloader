97bf2df9d489aa8338bafdebee5db949abd682d8
Дмитрий Карих <the.dr.hax@gmail.com>
2018-08-12 18:07:07 +0300

    Release 2.2
    * Refacroting
    * Added app.py for backward compatibility
    * Download all chats from channel

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='twitch-chat-downloader',
-      version='2.1.1',
+      version='2.2',
 
       author='Dmitry Karikh',
       author_email='the.dr.hax@gmail.com',
